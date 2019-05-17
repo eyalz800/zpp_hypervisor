a912ba760207589c741fd81d5de4aa106d8301d2
eyalz800 <eyalz800@gmail.com>
2019-05-17 18:34:31 +0300

    Major refactoring to project makefiles.

---
+++ linux_loader/zpp_project.mk
@@ -0,0 +1,88 @@
+ifeq ($(ZPP_PROJECT_SETTINGS), true)
+
+include ../environment.config
+
+ZPP_TARGET_NAME := zpp_loader.o
+ZPP_TARGET_TYPES := $(SUPPORTED_ARCHITECTURES)
+ZPP_LINK_TYPE := default
+ZPP_SOURCE_DIRECTORIES := \
+	../loader/src
+ZPP_SOURCE_FILES :=
+
+endif
+
+ifeq ($(ZPP_PROJECT_FLAGS), true)
+ZPP_FLAGS := \
+	$(patsubst %, -I%, $(shell find . -type d -name "include")) \
+	$(patsubst %, -I%, $(shell find ../hypervisor -type d -name "include")) \
+	-pedantic \
+	-Wall \
+	-Wextra \
+	-Werror \
+	-nostdlib \
+	-ffreestanding \
+	-mno-red-zone \
+	-DZPP_ELF_BINARY_PATH="\"../$(ZPP_OUTPUT_DIRECTORY)/zpp_hypervisor\""
+ZPP_FLAGS_DEBUG := \
+	-g
+ZPP_FLAGS_RELEASE := \
+	-O2 \
+	-ffunction-sections \
+	-fdata-sections \
+	-fvisibility=hidden
+ZPP_CFLAGS := \
+	$(ZPP_FLAGS) \
+	-std=c11
+ZPP_CFLAGS_DEBUG := \
+	$(ZPP_FLAGS_DEBUG)
+ZPP_CFLAGS_RELEASE := \
+	$(ZPP_FLAGS_RELEASE)
+ZPP_CXXFLAGS := \
+	$(ZPP_FLAGS) \
+	-std=c++17 \
+	-stdlib=libc++ \
+	-fno-rtti \
+	-fno-threadsafe-statics \
+	-fno-exceptions
+ZPP_CXXFLAGS_DEBUG := \
+	$(ZPP_FLAGS_DEBUG)
+ZPP_CXXFLAGS_RELEASE := \
+	$(ZPP_FLAGS_RELEASE)
+ZPP_ASFLAGS := \
+	$(ZPP_FLAGS) \
+	-x assembler-with-cpp
+ZPP_ASFLAGS_DEBUG := \
+	$(ZPP_FLAGS_DEBUG)
+ZPP_ASFLAGS_RELEASE := \
+	$(ZPP_FLAGS_RELEASE)
+ZPP_LFLAGS := \
+	$(ZPP_FLAGS) \
+	-r \
+	-Wl,--no-undefined
+ZPP_LFLAGS_DEBUG := \
+	$(ZPP_FLAGS_DEBUG)
+ZPP_LFLAGS_RELEASE := \
+	$(ZPP_FLAGS_RELEASE) \
+	-Wl,--strip-all \
+	-Wl,--gc-sections 
+endif
+
+ifeq ($(ZPP_PROJECT_RULES), true)
+
+$(ZPP_INTERMEDIATE_DIRECTORY)/../loader/src/elf_binary.o: \
+	../$(ZPP_OUTPUT_DIRECTORY)/zpp_hypervisor
+
+endif
+
+ifeq ($(ZPP_TOOLCHAIN_SETTINGS), true)
+
+ZPP_CC := clang
+ZPP_CXX := clang++
+ZPP_AS := $(ZPP_CC)
+ZPP_LINK := $(ZPP_CXX)
+ZPP_AR := ar
+
+-include zpp_toolchain.mk
+
+endif
+
---
+++ zpp.mk
@@ -0,0 +1,200 @@
+.SUFFIXES:
+.SECONDARY:
+.PHONY: \
+	all \
+	build \
+	build_single \
+	build_start_message \
+	build_mkdirs \
+	build_finished_message \
+	rebuild \
+	rebuild_single \
+	clean_mode \
+	clean \
+	clean_single
+
+mode ?= debug
+assembly ?= false
+
+ifeq ($(filter $(mode), debug release), )
+	$(error "Mode must either be debug or release.")
+endif
+
+ZPP_CONFIGURATION := $(mode)
+ZPP_GENERATE_ASSEMBLY := $(assembly)
+
+all: build
+ZPP_THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+ZPP_OUTPUT_DIRECTORY_ROOT := out
+ZPP_INTERMEDIATE_DIRECTORY_ROOT := obj
+
+ZPP_PROJECT_SETTINGS := true
+include zpp_project.mk
+ZPP_PROJECT_SETTINGS := false
+
+ifneq ($(ZPP_TARGET_TYPES), )
+build:
+	@for target_abi in $(ZPP_TARGET_TYPES); do \
+		$(MAKE) -s -f $(ZPP_THIS_MAKEFILE) ZPP_TARGET_TYPES= ZPP_TARGET_TYPE=$$target_abi; \
+	done
+clean:
+	@for target_abi in $(ZPP_TARGET_TYPES); do \
+		$(MAKE) -s -f $(ZPP_THIS_MAKEFILE) clean ZPP_TARGET_TYPES= ZPP_TARGET_TYPE=$$target_abi; \
+	done
+rebuild:
+	@for target_abi in $(ZPP_TARGET_TYPES); do \
+		$(MAKE) -s -f $(ZPP_THIS_MAKEFILE) rebuild ZPP_TARGET_TYPES= ZPP_TARGET_TYPE=$$target_abi; \
+	done
+else
+build: build_single
+clean: clean_single
+rebuild: rebuild_single
+
+ifeq ($(ZPP_SOURCE_DIRECTORIES), )
+ZPP_SOURCE_FILES := $(ZPP_SOURCE_FILES)
+else
+ZPP_SOURCE_FILES := $(ZPP_SOURCE_FILES) \
+	$(shell find $(ZPP_SOURCE_DIRECTORIES) -type f -name "*.S") \
+	$(shell find $(ZPP_SOURCE_DIRECTORIES) -type f -name "*.c") \
+	$(shell find $(ZPP_SOURCE_DIRECTORIES) -type f -name "*.cc") \
+	$(shell find $(ZPP_SOURCE_DIRECTORIES) -type f -name "*.cpp")
+endif
+
+ifeq ($(strip $(ZPP_SOURCE_FILES)), )
+$(error No source files)
+endif
+
+ZPP_INTERMEDIATE_DIRECTORY := $(ZPP_INTERMEDIATE_DIRECTORY_ROOT)/$(ZPP_CONFIGURATION)/$(ZPP_TARGET_TYPE)
+ZPP_OUTPUT_DIRECTORY := $(ZPP_OUTPUT_DIRECTORY_ROOT)/$(ZPP_CONFIGURATION)/$(ZPP_TARGET_TYPE)
+
+ZPP_PATH_FROM_ROOT := $(shell echo $(ZPP_SOURCE_FILES) | grep -o "\(\.\./\)*" | sort --unique | tail -n 1)
+ifneq ($(ZPP_PATH_FROM_ROOT), )
+ZPP_INTERMEDIATE_SUBDIRECTORY := $(shell realpath . --relative-to $(ZPP_PATH_FROM_ROOT))
+ZPP_INTERMEDIATE_DIRECTORY := $(ZPP_INTERMEDIATE_DIRECTORY)/$(ZPP_INTERMEDIATE_SUBDIRECTORY)
+endif
+
+ZPP_TOOLCHAIN_SETTINGS := true
+include zpp_project.mk
+ZPP_TOOLCHAIN_SETTINGS := false
+
+ZPP_PROJECT_FLAGS := true
+include zpp_project.mk
+ZPP_PROJECT_FLAGS := false
+
+ZPP_COMMA := ,
+ZPP_EMPTY :=
+ZPP_SPACE := $(ZPP_EMPTY) $(ZPP_EMPTY)
+
+ifeq ($(ZPP_CONFIGURATION), debug)
+ZPP_FLAGS := $(ZPP_FLAGS) $(ZPP_FLAGS_DEBUG)
+ZPP_CFLAGS := $(ZPP_CFLAGS) $(ZPP_CFLAGS_DEBUG)
+ZPP_CXXFLAGS := $(ZPP_CXXFLAGS) $(ZPP_CXXFLAGS_DEBUG)
+ZPP_ASFLAGS := $(ZPP_ASFLAGS) $(ZPP_ASFLAGS_DEBUG)
+ZPP_LFLAGS := $(ZPP_LFLAGS) $(ZPP_LFLAGS_DEBUG)
+else ifeq ($(ZPP_CONFIGURATION), release)
+ZPP_FLAGS := $(ZPP_FLAGS) $(ZPP_FLAGS_RELEASE)
+ZPP_CFLAGS := $(ZPP_CFLAGS) $(ZPP_CFLAGS_RELEASE)
+ZPP_CXXFLAGS := $(ZPP_CXXFLAGS) $(ZPP_CXXFLAGS_RELEASE)
+ZPP_ASFLAGS := $(ZPP_ASFLAGS) $(ZPP_ASFLAGS_RELEASE)
+ZPP_LFLAGS := $(ZPP_LFLAGS) $(ZPP_LFLAGS_RELEASE)
+endif
+
+ZPP_OBJECT_FILES := $(patsubst %.c, %.o, $(ZPP_SOURCE_FILES))
+ZPP_OBJECT_FILES := $(ZPP_OBJECT_FILES) $(patsubst %.cpp, %.o, $(ZPP_SOURCE_FILES))
+ZPP_OBJECT_FILES := $(ZPP_OBJECT_FILES) $(patsubst %.cc, %.o, $(ZPP_SOURCE_FILES))
+ZPP_OBJECT_FILES := $(ZPP_OBJECT_FILES) $(patsubst %.S, %.o, $(ZPP_SOURCE_FILES))
+ZPP_OBJECT_FILES := $(patsubst %, $(ZPP_INTERMEDIATE_DIRECTORY)/%, $(ZPP_OBJECT_FILES))
+ZPP_OBJECT_FILES := $(filter %.o, $(ZPP_OBJECT_FILES))
+ZPP_OBJECT_FILES_DIRECTORIES := $(dir $(ZPP_OBJECT_FILES))
+
+ZPP_DEPENDENCY_FILES := $(patsubst %.o, %.d, $(ZPP_OBJECT_FILES))
+
+ifeq ($(ZPP_LINK_TYPE), default)
+	ZPP_LINK_COMMAND := $(ZPP_LINK) -o $(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME) $(ZPP_OBJECT_FILES) $(ZPP_LFLAGS)
+else ifeq ($(ZPP_LINK_TYPE), ld)
+	ZPP_LINK_COMMAND := $(ZPP_LINK) -o $(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME) $(ZPP_OBJECT_FILES) $(ZPP_LFLAGS)
+else ifeq ($(ZPP_LINK_TYPE), link)
+	ZPP_LINK_COMMAND := $(ZPP_LINK) $(ZPP_LFLAGS) /out:$(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME) $(ZPP_OBJECT_FILES)
+else ifeq ($(ZPP_LINK_TYPE), ar) 
+	ZPP_LINK_COMMAND := $(ZPP_AR) rcs $(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME) $(ZPP_OBJECT_FILES)
+else
+	$(error "ZPP_LINK_TYPE must either be default, ld, link, or ar.")
+endif
+
+build_single: build_finished_message
+
+build_start_message:
+	@echo "Building '$(ZPP_TARGET_TYPE)/$(ZPP_TARGET_NAME)' in '$(ZPP_CONFIGURATION)' mode..."
+
+build_finished_message: $(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME)
+	@echo "Built '$(ZPP_TARGET_TYPE)/$(ZPP_TARGET_NAME)'."
+
+build_mkdirs: build_start_message
+	@mkdir -p $(ZPP_INTERMEDIATE_DIRECTORY); \
+	mkdir -p $(ZPP_OUTPUT_DIRECTORY); \
+	mkdir -p $(ZPP_OBJECT_FILES_DIRECTORIES)
+
+$(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME): $(ZPP_OBJECT_FILES)
+	@echo "Linking '$(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME)'..."; \
+	$(ZPP_LINK_COMMAND)
+
+ifeq ($(ZPP_GENERATE_ASSEMBLY), true)
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.S: %.c | build_mkdirs
+	@echo "Compiling '$<'..."; \
+	$(ZPP_CC) -S $(ZPP_CFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .S`.d
+
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.S: %.cpp | build_mkdirs
+	@echo "Compiling '$<'..."; \
+	$(ZPP_CXX) -S $(ZPP_CXXFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .S`.d
+
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.S: %.cc | build_mkdirs
+	@echo "Compiling '$<'..."; \
+	$(ZPP_CXX) -S $(ZPP_CXXFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .S`.d
+	
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.o: $(ZPP_INTERMEDIATE_DIRECTORY)/%.S
+	@$(ZPP_CC) -s -Wno-unicode -c -o $@ $<
+else ifeq ($(ZPP_GENERATE_ASSEMBLY), false)
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.o: %.c | build_mkdirs
+	@echo "Compiling '$<'..."; \
+	$(ZPP_CC) -c $(ZPP_CFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .o`.d
+
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.o: %.cpp | build_mkdirs
+	@echo "Compiling '$<'..."; \
+	$(ZPP_CXX) -c $(ZPP_CXXFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .o`.d
+
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.o: %.cc | build_mkdirs
+	@echo "Compiling '$<'..."; \
+	$(ZPP_CXX) -c $(ZPP_CXXFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .o`.d
+else
+$(error "ZPP_GENERATE_ASSEMBLY must either be true or false.")
+endif
+
+$(ZPP_INTERMEDIATE_DIRECTORY)/%.o: %.S | build_mkdirs
+	@echo "Assemblying '$<'..."; \
+	$(ZPP_AS) -c $(ZPP_ASFLAGS) -o $@ $< -MD -Wp,-MD,`dirname $@`/`basename $@ .o`.d
+
+-include $(ZPP_DEPENDENCY_FILES)
+
+clean_single:
+	@echo "Cleaning '$(ZPP_TARGET_TYPE)/$(ZPP_TARGET_NAME)'..."; \
+	rm -rf $(ZPP_INTERMEDIATE_DIRECTORY_ROOT)/debug/$(ZPP_TARGET_TYPE); \
+	rm -rf $(ZPP_INTERMEDIATE_DIRECTORY_ROOT)/release/$(ZPP_TARGET_TYPE); \
+	rm -f $(ZPP_OUTPUT_DIRECTORY_ROOT)/debug/$(ZPP_TARGET_TYPE)/$(ZPP_TARGET_NAME); \
+	rm -f $(ZPP_OUTPUT_DIRECTORY_ROOT)/release/$(ZPP_TARGET_TYPE)/$(ZPP_TARGET_NAME); \
+	find $(ZPP_INTERMEDIATE_DIRECTORY_ROOT) -type d -empty -delete 2> /dev/null; \
+	find $(ZPP_OUTPUT_DIRECTORY_ROOT) -type d -empty -delete 2> /dev/null; \
+	echo "Cleaned '$(ZPP_TARGET_TYPE)/$(ZPP_TARGET_NAME)'."
+
+clean_mode: 
+	@rm -rf $(ZPP_INTERMEDIATE_DIRECTORY); \
+	rm -rf $(ZPP_OUTPUT_DIRECTORY)/$(ZPP_TARGET_NAME)
+
+rebuild_single: clean_mode
+	@$(MAKE) -s -f $(ZPP_THIS_MAKEFILE) build
+
+ZPP_PROJECT_RULES := true
+include zpp_project.mk
+ZPP_PROJECT_RULES := false
+
+endif
+
